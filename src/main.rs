ee7d4045f6a087441050d328674c62ff7350bfd0
mtcl <moticul00@pm.me>
2025-05-27 07:45:34 -0400

    figured out how to extract specifi items from the Disks struct

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-mod cli;
+use comfy_table::Table;
+use sysinfo::Disks;
+
+fn main() {
+    let disks = Disks::new_with_refreshed_list();
+    for disk in &disks {
+        println!("{:?}", disk.name());
+        println!("{:?}", disk.mount_point());
+    }
+
+    let mut table = Table::new();
+    table
+        .set_header(vec!["Disk Name", "Disk Kind", "Filsystem"])
+        .add_row(vec!["", "This is another text", "This is the third text"])
+        .add_row(vec![
+            "This is another text",
+            "Now\nadd some \nmulti line stuff",
+            "This is awesome",
+        ]);
+
+    println!("{table}");
+}
